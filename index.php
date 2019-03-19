dc7594353d319478faf3c00cf9bef564a1ad729a
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2019-03-19 10:03:05 -0600

    ADD: Se añade el index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	echo "<h1>Bienvenido a mi examen.</h1>"
+	echo "<br>"
+	echo "<p>Esto es de temática libre, así que hablaré de perros.</p>"
+	echo "<br>"
+	echo "<h1>Mi perro es bien bonito, ojalá lo veas.</h1>"
+	echo "<br>"
+?>
